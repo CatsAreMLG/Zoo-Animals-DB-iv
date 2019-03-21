911b0564db694cca8f34d2bdfdab0c132a6c7cb9
CatsAreMLG <joshuadgon@gmail.com>
2019-03-20 21:51:00 -0500

    Change species to species_name

--- data/migrations/20190320214214_createSpeciesTable.js
+++ data/migrations/20190320214214_createSpeciesTable.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('species', tbl => {
     tbl.increments()
     tbl
-      .string('species', 128)
+      .string('species_name', 128)
       .notNullable()
       .unique()
   })
